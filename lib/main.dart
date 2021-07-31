4f92a232c329fe024fc4eb2aaf87e11efb572457
tokitsu <toki2222son@yahoo.co.jp>
2021-07-31 10:11:10 +0900

    add provider4.1.3

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,38 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  final String kboyText = 'Kboysan';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  final myFocusNode = FocusNode();
-
-  final items = List<String>.generate(10000, (i) => "Item $i");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('コリアンダー'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'not kboys'
-            )
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('コリアンダー'),
         ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                kboyText,
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Button'),
+                onPressed: () {
+
+                },
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
 }
+
+
